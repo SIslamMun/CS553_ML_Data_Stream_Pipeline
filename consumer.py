67b4ba4e86d36ce1386975deaa3adb722a39f9a8
vpapudesibabu <vpapudesibabu@hawk.iit.edu>
2023-11-05 10:08:42 -0600

    Support image file in message queue
    Includes:
    Producer can take images as the user request and push to queue.
    Consumer can read the image and store in directory  'received_images'
    Eliminated print statements and introduced logging.

--- consumer.py
+++ consumer.py
@@ -1,7 +1,17 @@
 import pika
 import configparser
+import logging
+import sys
+import os
+import base64
+import time
 
+stream_handler = logging.StreamHandler(sys.stdout)
+log_formatter = logging.Formatter(
+    '%(asctime)s - %(levelname)s - %(name)s - %(message)s')
+stream_handler.setFormatter(log_formatter)
 
+logger = logging.getLogger("pipeline")
 
 def load_config(config_file="config.ini"):
     config = configparser.ConfigParser()
@@ -15,7 +25,14 @@ queue = config.get("rabitmq", "queue")
 
 
 def callback(ch, method, properties, body):
-    print(f"Received: {body}")
+    image_data = body.decode()
+    image_bytes = base64.b64decode(image_data)
+    # print(f"Received: {body}")
+    timestamp = int(time.time())
+    filename = f"received_image_{timestamp}.png"
+    image_path = os.path.join('received_images', filename)  # You can change the file format as needed
+    with open(image_path, 'wb') as image_file:
+        image_file.write(image_bytes)
 
 # Connect to RabbitMQ server
 connection = pika.BlockingConnection(pika.ConnectionParameters(rabbitmq_host))
@@ -25,5 +42,5 @@ channel.queue_declare(queue=queue)
 
 channel.basic_consume(queue=queue, on_message_callback=callback, auto_ack=True)
 
-print("Waiting for messages. To exit, press CTRL+C")
+logger.info("Waiting for messages. To exit, press CTRL+C")
 channel.start_consuming()
--- producer.py
+++ producer.py
@@ -1,10 +1,20 @@
+import base64
 from flask import Flask, request, jsonify
 import configparser
-
+import logging
 import pika
+import sys
 
 app = Flask(__name__)
 
+stream_handler = logging.StreamHandler(sys.stdout)
+log_formatter = logging.Formatter(
+    '%(asctime)s - %(levelname)s - %(name)s - %(message)s')
+stream_handler.setFormatter(log_formatter)
+log_level = logging.INFO #TODO later
+logger = logging.getLogger("pipeline")
+logger.setLevel(log_level)
+logger.addHandler(stream_handler)
 
 def load_config(config_file="config.ini"):
     config = configparser.ConfigParser()
@@ -23,24 +33,28 @@ channel = connection.channel()
 def createQueue(channel, queue):
     try:
         channel.queue_declare(queue=queue, passive=True)
-        print(f"Queue '{queue}' already exists.")
+        logger.info(f"Queue '{queue}' already exists.")
     except pika.exceptions.ChannelClosed as e:
         # Queue does not exist, create it
         channel.queue_declare(queue=queue)
-        print(f"Queue '{queue}' created.")
+        logger.info(f"Queue '{queue}' created.")
 
 @app.route('/produce', methods=['POST'])
 def produce_message():
     try:
-        message = request.json['message']
-        
-
-        channel.basic_publish(exchange='', routing_key=queue, body=message)
-
+        # message = request.json['message']
+        message = request.files.get('image')
+        if message:
+            image_data = base64.b64encode(message.read()).decode()
+            channel.basic_publish(exchange='', routing_key=queue, body=image_data)
         # Close the connection
-        connection.close()
+            connection.close()
+            logger.info("Pushed image to the queue")
+            return jsonify({"status": "Message sent successfully"})
+        else:
+            logger.info("Image not provided in the request input")
+            return jsonify({"status": "request failed"})
 
-        return jsonify({"status": "Message sent successfully"})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
